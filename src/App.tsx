8adc2ab41ff9891d43e1fdc73d415a52c614d966
Frederico Filho <frederico.filho@rethink.dev>
2022-04-11 15:37:01 -0300

    Projeto React criado/ conexao com API / Mapa Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class App extends React.Component {
+  defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  };
+
+  render() {
+    return (
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyBsVYbXTR0-3uuTQZBIb60wUDuvZXxXTOI" }}
+          defaultCenter={this.defaultProps.center}
+          defaultZoom={this.defaultProps.zoom}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {/* <AnyReactComponent lat={59.955413} lng={30.337844} text='My Marker' /> */}
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
-
 export default App;
